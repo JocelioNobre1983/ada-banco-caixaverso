7bc5a55505eda670eadaece195802a97dbe6087b
JocelioNobre1983 <JocelioNobre1983>
2026-03-04 17:36:52 -0300

    "primeiro commit - criando programa"

---
+++ src/main/java/com/ada/caixaverso/model/Transacoes.java
@@ -0,0 +1,55 @@
+package com.ada.caixaverso.model;
+
+import java.time.LocalDateTime;
+
+public class Transacoes {
+    private String agencia;
+    private String conta;
+    private String banco;
+    private String titular;
+    private String operacao; //"SAQUE" OU "DEPÓSITO"
+    private double valor;
+    private String operador;
+    private LocalDateTime datahora;
+
+    //CONSTRUTORES
+    public Transacoes() {
+    }
+
+    public Transacoes(String agencia, String conta, String banco, String titular,
+                     String operacao, double valor, String operador,
+                      LocalDateTime datahora) {
+
+        this.agencia = agencia;
+        this.conta = conta.trim();
+        this.banco = banco;
+        this.titular = titular.toUpperCase().trim();
+        this.operacao = operacao.toUpperCase().trim();
+        this.valor = valor;
+        this.operador = operador;
+        this.datahora = datahora;
+    }
+
+    //GETTERS
+    public String getAgencia() { return agencia; }
+
+    public String getConta() { return conta; }
+
+    public String getBanco() { return banco; }
+
+    public String getTitular() { return titular; }
+
+    public String getOperacao() { return operacao; }
+
+    public double getValor() { return valor; }
+
+    public String getOperador() { return operador; }
+
+    public LocalDateTime getDatahora() { return datahora; }
+
+    @Override
+    public String toString(){
+        return String.format("[%s] %-10s R$ %10.2f | Operador: %-15s | Conta: %s-%s (%s)",
+                datahora, operacao, valor, operador, agencia, conta, banco);
+    }
+}
